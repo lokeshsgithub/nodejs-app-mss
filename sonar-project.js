e59f6029ea986580081dd91b0ffd7e2331d94dbf
Lokesh <lokeshreddy05690@gmail.com>
2023-01-29 12:33:09 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://172.31.2.189:9182/lokeshreddy/',
+  serverUrl: 'http://172.31.35.188:9182/lokeshreddy/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_8136cdc3c227e470816a612b07c0ba8d17b6031a',
+	    'sonar.login': 'squ_e83b9ce60210de27361b5efb8f69bc7aa99a1236',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
